7ca32ec3fd78ce259c6450f5474b2afa77eee1de
Mimik <76145389+Saijoer@users.noreply.github.com>
2024-03-10 18:10:30 +0500

    Update main.go

--- Back-end/main.go
+++ Back-end/main.go
@@ -1,47 +1,51 @@
 package main
 
 import (
+	"html/template"
+	"io"
 	"net/http"
 
 	"github.com/labstack/echo"
 )
 
+type TemplateRenderer struct {
+	templates *template.Template
+}
+
+func (t *TemplateRenderer) Render(w io.Writer, name string, data interface{}, c echo.Context) error {
+	return t.templates.ExecuteTemplate(w, name, data)
+}
+
 func main() {
 	e := echo.New()
 
-	// Определяем маршруты и их обработчики
+	e.Static("/public", "public")
+
+	renderer := &TemplateRenderer{
+		templates: template.Must(template.ParseGlob("templates/*.html")),
+	}
+	e.Renderer = renderer
+
+	// Маршруты
 	e.GET("/home", homeHandler)
 	e.GET("/login", loginHandler)
 	e.GET("/register", registerHandler)
 
-	// Запускаем сервер на порту 8080
+	// Запуск сервера
 	e.Logger.Fatal(e.Start(":8080"))
 }
 
 // Обработчик для домашней страницы
 func homeHandler(c echo.Context) error {
-	html := `
-    <!DOCTYPE html>
-    <html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <title>Home Page</title>	
-    </head>
-    <body>
-        <h1>Welcome to the Home Page!</h1>
-        <a href="/login">Login</a>
-    </body>
-    </html>
-    `
-	return c.HTML(http.StatusOK, html)
+	return c.Render(http.StatusOK, "home.html", nil)
 }
 
 // Обработчик для страницы входа в аккаунт
 func loginHandler(c echo.Context) error {
-	return c.String(http.StatusOK, "Страница для входа в аккаунт")
+	return c.Render(http.StatusOK, "login.html", nil)
 }
 
 // Обработчик для страницы регистрации
 func registerHandler(c echo.Context) error {
-	return c.String(http.StatusOK, "Страница для регистрации")
+	return c.Render(http.StatusOK, "register.html", nil)
 }
